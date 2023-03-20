0f460c7e0170e03638c30e15139b55e2a27366bf
lmarotte <leo-paul.marotte@ensae.fr>
2023-03-20 09:20:38 +0000

    ok

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import matplotlib.pyplot as plt
+
+
+
+
+
+
 def names(year1, year2):
     dico = {}
     for i in range(year1, year2):
@@ -13,5 +20,27 @@ def names(year1, year2):
                 nom = ligne[0]
                 nb = int(ligne[2])
     print(dico)
+
+
+def f(alpha, beta, gamma, N):
+    P =
+    K = P*(alpha+beta)/(alpha + beta + gamma)
+    return (alpha + beta)*N*(1 - N/K)
+
+
+
+
+def Euler(f,a,t0,T,n) : 
+    dt=T/n
+    t=[t0+i*dt for i in range(n+1)]
+    y=[a]
+    
+    for i in range(1,n+1):
+        y . append ( y [ i - 1 ] + dt * f ( y [ i - 1 ] ) )
+    plt.plot(t,y) plt.show()
+
+
+#def f(x,t) : #fonction f pour tester
+    #return x
 
 
